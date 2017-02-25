f2e95e8013d8faeb07469c9cb816a703ada2f458
Steve Goguen <steve.goguen@alliedbuilding.com>
2017-02-25 13:03:48 -0500

    Added Active-Patterns snip

---
+++ snips/active-patterns.fsx
@@ -0,0 +1,22 @@
+//  Loosly speaking, Active patterns allow you to create
+//  functions that are good at extracting structured
+//  data.
+
+//  Let make one.
+
+//  I'm going to need my Banana clips (|  |)
+
+open System
+
+let (|Odd|Even|String|)(s:string) = 
+    let (isInt, value) = Int32.TryParse(s)
+    if isInt then
+       if value % 2 = 1 then Odd(value)
+       else Even(value)
+    else String(s)
+
+let tellMeAbout(s:string) = 
+    match s with
+    | Odd(value) -> sprintf "%i is odd!" value
+    | Even(value) -> sprintf "%i is even!" value
+    | String(value) -> sprintf "%s is a string!" value
